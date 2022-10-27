9f1a218422f5c08f7caef329f6a48c2b24fea591
Mansur <mansuribrahimnok@gmail.com>
2022-10-27 23:46:34 +0100

    Testing packages configured

---
+++ api/test/test.js
@@ -0,0 +1,28 @@
+require ('chai').should()
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+//import './chai/register-should';
+import app from '../index';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Testing the class-manager endpoints:', () => {
+
+
+  it('It should get a welcome message at the root endpoint', (done) => {
+    const bookId = 1;
+    chai.request(app)
+      .get("/")
+      .set('Accept', 'application/json')
+      .then((res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.data.message).to.include("Welcome to this API.")
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      done();
+  });
+
+});
