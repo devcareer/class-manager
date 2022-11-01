f27fea1bb44495f7dde65b552a30395bc9b8fb10
Tjan <tijanayomide@yahoo.com>
2022-11-01 21:41:51 +0000

    added github workflow, eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 
-
 const app = express();
 dotenv.config();
 
@@ -13,6 +12,5 @@ app.get("/ping", (_req, res) => {
     res.status(200);
     return res.json("Pong")
 });
-
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import app from "../src/index.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
